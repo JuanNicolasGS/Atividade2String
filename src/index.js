e5023d4bde8b5721ec8588749eed9c82ce910e04
Juan Nicolas <juan.nicolas@academico.ifpb.edu.br>
2025-05-15 17:35:34 -0300

    :lock: retirando o innerHTML que é vulneravel

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ let cardTitle5 = document.getElementById('question5Title');
 var string = input.value;
 var value = number.value;
 
-cardTitle1.innerHTML = isString(string);
-cardTitle2.innerHTML = isEmpty(string);
-cardTitle3.innerHTML = toArray(string);
-cardTitle4.innerHTML = toCut(string, value);
-cardTitle5.innerHTML = toAbbrev(string);
+cardTitle1.textContent = isString(string);
+cardTitle2.textContent = isEmpty(string);
+cardTitle3.textContent = toArray(string);
+cardTitle4.textContent = toCut(string, value);
+cardTitle5.textContent = toAbbrev(string);
